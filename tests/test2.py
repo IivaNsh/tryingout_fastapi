cc45f836559bc1479157bb0b1833c466c8d49235
IivaNsh <ivansher0226@gmail.com>
2024-05-20 17:15:39 +0300

    updating tests

--- tests/test2.py
+++ tests/test2.py
@@ -8,7 +8,7 @@ files = {
     'file': open('test_file.json', 'rb'),
 }
 
-response = requests.post('http://127.0.0.1:8000/uploadfile', files=files)
+response = requests.post('http://127.0.0.1:8000/save_file', files=files)
 
 print(response.status_code)
 print(response.content)
---
+++ tests/test3.py
@@ -0,0 +1,2 @@
+with open('saved_files/readme.txt', 'xb') as f:
+    f.write(b'Create a new text file!')
