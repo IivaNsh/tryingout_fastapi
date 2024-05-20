913ec6b117e25edff8d9a2511dd3b5e75daede96
IivaNsh <ivansher0226@gmail.com>
2024-05-20 17:14:37 +0300

    adding save_file endpoint

--- app.py
+++ app.py
@@ -1,21 +1,50 @@
 from typing import Union, Annotated
 
 from fastapi import FastAPI, File, UploadFile
+from fastapi.responses import HTMLResponse
+
+from utils import get_datatime_str as GetDate, split_filename as SplitFilename
+
+
 
 app = FastAPI()
 
 @app.get("/")
-def read_root():
-    return {"Hello": "World"}
+async def main():
+    content = """
+<body>
+<form action="/files/" enctype="multipart/form-data" method="post">
+<input name="files" type="file" multiple>
+<input type="submit">
+</form>
+<form action="/uploadfiles/" enctype="multipart/form-data" method="post">
+<input name="files" type="file" multiple>
+<input type="submit">
+</form>
+</body>
+    """
+    return HTMLResponse(content=content)
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+# @app.get("/items/{item_id}")
+# def read_item(item_id: int, q: Union[str, None] = None):
+#     return {"item_id": item_id, "q": q}
 
-@app.post("/files/")
-async def create_file(file: Annotated[bytes, File()]):
-    return {"file_size": len(file)}
+# @app.post("/files/")
+# async def create_file(file: Annotated[bytes, File()]):
+#     return {"file_size": len(file)}
 
-@app.post("/uploadfile/")
-async def create_upload_file(file: UploadFile):
-    return {"filename": file.filename}
+# @app.post("/uploadfile/")
+# async def create_upload_file(file: UploadFile):
+#     return {"filename": file.filename}
+
+@app.post("/save_file/")
+async def save_upload_file(file: UploadFile):
+    contents = await file.read()
+    
+    file_name, file_ext = SplitFilename(file.filename)
+    save_name = f"{file_name}{GetDate()}{file_ext}"
+
+    with open(f"saved_files/{save_name}", "wb") as f:
+        f.write(contents)
+
+    return {"file_saved": save_name}
